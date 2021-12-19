4c214405992d4048f572cb260682bc8192d5b50d
benedict-vimdey <bekudzordzi001@st.ug.edu.gh>
2021-12-19 23:52:51 +0000

    Added the hero page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Herosection from './Components/Herosection';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className=" bg-white h-screen">
      <Navbar/>
+     <Herosection/>
     </div>
   );
 }
--- src/Components/Herosection.js
+++ src/Components/Herosection.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import {ReactComponent as BTC} from '../Images/bitcoin-blue.svg'
+import {ReactComponent as Arrow} from '../Images/right-arrow.svg'
+import {ReactComponent as Phone} from '../Images/phone.svg'
 
 function Herosection() {
     return (
-        <div>
-            <div></div>
+        <div className='grid grid-cols-2 mx-auto w-full mt-15 px-6 lg:max-w-6xl pb-10 pt-20'>
+            <div className='flex'>
+                <div className='mx-auto my-10'>
+                    <div className='flex'>
+                      <BTC/>
+                        <p className='mx-2 font-semibold hover:underline text-blue-800'>
+                          Jump Start your portfolio
+                        </p>
+                      <Arrow className='my-auto'/>
+                    </div>
+                    
+                    <p className='font-semibold text-3xl max-w-md mb-2 mt-5 md:mt-2 leading-tight' style={{fontSize:'62px'}}>Jump start your crypto portfolio</p>
+                    <p className='font-semibold leading-tight'>
+                    Coinbase is the easiest place to buy and sell <br/> cryptocurrency. Sign up and get started today.
+                    </p>
+
+                    <div>
+                        <input type='text' placeholder='Email address' className=' transition duration-250 bg-white w-min border border-gray-400 px-4 py-5 mt-10 hover:border-black' style={{borderRadius:'4px'}} />
+                        <button className=' bg-blue-700 ml-3 text-white w-fit inline-block font-semibold px-8 py-5' style={{borderRadius:'4px'}}>Get Started</button>
+                    </div>
+
+                    
+                </div>
+
+            </div>
+            <div className='w-full '>
+                <Phone className='w-4/5 ml-10' />
+            </div>
         </div>
     )
 }
